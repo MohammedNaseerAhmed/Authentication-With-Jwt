34da9990b188111734245f71ae2ab229d3eaeb4a
MohammedNaseerAhmed <naseer9392517184@gmail.com>
2025-09-01 22:56:02 +0530

    userCountroller

--- server/controllers/userCountroller.js
+++ server/controllers/userCountroller.js
@@ -2,17 +2,21 @@ import userModel from "../models/userModel.js"
 
 export const getUserData=async(req,res)=>{
     try{
+        if (!req.userId) {
+            return res.status(401).json({ success: false, message: "Unauthorized " });
+        }
+
         const userId=req.userId;
         const user= await userModel.findById(userId);
         if (!user){
             return res.json({success:false,message:"User not found"})
         }
 
-res.json({
-    success:true,
-    userData:{
-        name:user.name,
-        isAccountVerified:user.isAccountVerified,
+    res.json({
+        success:true,
+        userData:{
+            name:user.name,
+            isAccountVerified:user.isAccountVerified,
     }
 })
 
